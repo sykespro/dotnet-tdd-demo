3e11654fd0d149e8a76de6f4dcb70930e7234b1b
Daniel Sykes <sykespro@gmail.com>
2022-05-05 09:45:49 -0400

    feat: Add new room identifier

---
+++ TDD/Room.cs
@@ -0,0 +1,21 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TDD
+{
+    public class Room
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        public String RoomType { get; set; }
+
+        public int CurrentCapacity { get; set; }
+
+        public int MaxCapacity { get; set; }
+
+        public string RoomIdentifier { get; set; }
+    }
+}
